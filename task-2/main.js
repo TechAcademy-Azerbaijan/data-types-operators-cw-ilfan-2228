79a66a5d878383315059f8d977bb37782f0cbba8
Ilfan Feyzullazade <feyzullazaddilfan@gmail.com>
2022-08-09 13:24:32 +0400

    HomeWork-2 Ilfan

--- task-2/main.js
+++ task-2/main.js
@@ -5,8 +5,11 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
 let a = parseInt(result.input)  
+
 let s = 6*(a*2)
+
 let v = a**3
+
 console.log(s,v)
 
 });
--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,14 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+var input = result.input;
+var numbers = input.split(',')  
+var firstNumber = parseInt(numbers[0]);
+var secondNumber = parseInt(numbers[1]);
+var resultNumber = firstNumber**secondNumber
+console.log(resultNumber);
+
+
+
+
 });
